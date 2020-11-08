e993c69206816af5ae02a80182ba62537f4eeb44
aadamko1 <aadamko1@emich.edu>
2020-11-08 21:39:35 +0000

    added boxes,email requests, and paging to webpage

--- app.py
+++ app.py
@@ -1,17 +1,52 @@
 import query_on_whoosh
 from flask import Flask, render_template, request, jsonify
+import smtplib
+import config
+import math
 
+# turn this file (app.py) into a web app
 app = Flask(__name__)
 app.config.update(dict(JSONIFY_PRETTYPRINT_REGULAR=True))
 
+# I want to build an app that has a route that listens to /
 @app.route("/")
 def handle_slash():
-    user_name = request.args.get("name")
-    return render_template("index.html", name=user_name)
+    requested_name = request.args.get("name")
+    return render_template("index.html", user_name=requested_name)    
 
-@app.route("/query/")
+@app.route("/query", strict_slashes=False)
 def handle_query():
-    search_term = request.args.get("q")
-    page_num = int(request.args.get("p"))
-    search_results, num_hits,num_pages=query_on_whoosh.query(search_term, search_page, 10)
-    return jsonify({"query_term":search_term, "num_hits":num_hits, "num_pages": num_pages, "search_results":search_results})
+    query_term = request.args.get("q")
+    page_index = int(request.args.get("p"))
+    return jsonify({"query_term": query_term, "search_results": query_on_whoosh.query(query_term, current_page=page_index)})
+
+@app.route("/query_view", strict_slashes=False)
+def handle_query_view():
+    query_term = request.args.get("q")
+    if not query_term:
+        query_term = ""
+
+    page_index_arg = request.args.get("p")
+    if not page_index_arg:
+        page_index_arg = "1"
+
+    page_index = int(page_index_arg)
+    query_results = query_on_whoosh.query(query_term, current_page=page_index)
+    search_results = query_results[0] #returns a page of the search results
+    results_cnt = query_results[1] #returns a total number of the search results
+    page_cnt = math.ceil(results_cnt / 10 )
+    return render_template("query.html", results = search_results, page_cnt=page_cnt, query_term=query_term)
+
+@app.route("/about", strict_slashes=False)
+def handle_about():
+    return render_template("about.html")
+
+@app.route("/success", strict_slashes=False)
+def handle_request():
+    new_data = request.args.get("new_data")
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login("akowski08@gmail.com", config.gmail_password)
+    message = 'Subject: {}\n\n{}'.format("Request to add new data", "request to add: " + new_data)
+    server.sendmail("akowski08@gmail.com", "aadamko1@emich.edu", message)
+    return render_template("success.html", new_data=new_data)
--- query_on_whoosh.py
+++ query_on_whoosh.py
@@ -1,37 +1,33 @@
-import sys
-import json
-
-#term --> object
-#"home and child" --> searches for document that contains
-#both home and child (but not necessarily only in that order)
-#***"and"*** is a stop word, wont query
-
 from whoosh.qparser import QueryParser
 from whoosh import scoring
 from whoosh.index import open_dir
+import sys
+import json
 
+ix = open_dir("indexdir")
 
-def query(query_str, page_Number, items_per_page):
-    ix = open_dir("indexdir")
+def query(query_str, items_per_page=10, current_page=1):
     with ix.searcher(weighting=scoring.Frequency) as searcher:
         query = QueryParser("description", ix.schema).parse(query_str)
         results = searcher.search(query, limit=None)
         num_query_results = len(results)
         query_results = []
-        
+        start_index = (current_page - 1) * items_per_page
+        end_index = start_index + items_per_page
 
         for i in range(start_index, min(len(results), end_index)):
-            d = {}
+            d={}
             d['url'] = "https://www.youtube.com/watch?v=%s" % results[i]['id']
             d['title'] = results[i]['title']
-            d['description'] = results[i]['description']
+            d['description'] = results[i].highlights('description')
             d['score'] = results[i].score
             query_results.append(d)
-    
-    return query_results, num_query_results
+
+        return query_results, num_query_results
 
 if __name__ == "__main__":
     query_str = sys.argv[1]
-    page_Number = int(sys.argv[2])
-    items_per_page = int(sys.argv[3])
-    query(query_str, page_Number, items_per_page)
+    items_per_page = int(sys.argv[2])
+    current_page = int(sys.argv[3])
+    query_results, num_query_results = query(query_str, items_per_page=items_per_page, current_page=current_page)
+    print(json.dumps(query_results, indent=4))
